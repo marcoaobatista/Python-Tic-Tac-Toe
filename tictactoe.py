65b4d64fd65e82b9dd47520d380d9f6fe66e425b
Marco Antônio <marco.o.batista@gmail.com>
2023-06-02 19:57:31 -0300

    Scoreboard now player based instead of x or o

--- tictactoe.py
+++ tictactoe.py
@@ -17,6 +17,7 @@
 #         if it was draw, display it
 #         otherwise, display winner and give them a point
 #         prompt user if they want to continue
+#         change starting player
 #     display final scoreboard and goodbye message
 ############################################################
 """
@@ -26,7 +27,7 @@ class TicTacToe(object):
         self.board = [[" " for i in range(3)] for j in range(3)]
         # Set current player
         self.turn = "x"
-        # map keyboard keys to coordinates in the game board
+        # Map keyboard keys to coordinates in the game board
         self.hash = {"t":(0,0),"y":(0,1),"u":(0,2),
                      "g":(1,0),"h":(1,1),"j":(1,2),
                      "b":(2,0),"n":(2,1),"m":(2,2)}
@@ -73,7 +74,7 @@ ended, and the second item being who won, or if it was a draw
             elif lst.count("o") == 3:
                 return True, "o"
             
-        # check if it was a draw
+        # Check if it was a draw
         if uoboard.count("x") + uoboard.count("o") == 9:
             return True, "draw"
         
@@ -101,16 +102,19 @@ ended, and the second item being who won, or if it was a draw
         self.turn = "o" if self.turn == "x" else "x"
         
 
-    def take_input(self):
-        """ Prompts and re-prompts for a key in the board to lay x or o and 
-then returns it """
-        key = input(self.turn + "'s turn: ")
+    def take_input(self, cur_player):
+        """ 
+        Prompts and re-prompts for a key in the board to lay x or o and 
+then returns it 
+        cur_player: The player that is going to play next
+        """
+        key = input("Player {}'s ({}) turn: ".format(cur_player, self.turn))
         # Re prompt while it is not a valid key or if place is already filled
         while key not in self.hash or \
                 self.board[self.hash[key][0]][self.hash[key][1]] != " ":
             print("\nInvalid key, try again")
             self.display_board()
-            key = input(self.turn + "'s turn: ")
+            key = input("Player {}'s ({}) turn: ".format(cur_player, self.turn))
         return key
                 
     
@@ -133,13 +137,18 @@ then returns it """
         print("{:^3}|{:^3}|{:^3}".format(*mapped_keys[6:]))
 
 
+def opposite_player(player_turn):
+    """ Returns player opposite to the input (1 or 2) """
+    return 2 if player_turn == 1 else 1
+
 def main():
     game = TicTacToe()
     game.display_welcome_message()
     
-    scoreboard = {"x":0,"o":0}
+    scores = {1:0,2:0}
     cont = "y"
-    
+    starting_player = 1
+    cur_player = 1
     while cont.lower() == "y":
         print("\n= NEW GAME =")
         game = TicTacToe()
@@ -147,22 +156,33 @@ def main():
         has_ended, winner = game.check_win()
         
         while not has_ended:
-            user_input = game.take_input()
+            user_input = game.take_input(cur_player)
             game.make_play(user_input)
             game.display_board()
             has_ended, result = game.check_win()
+            cur_player = opposite_player(cur_player)
             
         if result == "draw":
             print("\nTHAT'S A DRAW!")
         else:
-            print("\n"+result.upper()+" WON! CONGRATULATIONS!!!")
-            scoreboard[result] += 1
+            print("\n"+result.upper()+" WON. CONGRATULATIONS!")
+            if result == "x":
+                scores[starting_player] += 1
+            else:
+                scores[opposite_player(starting_player)] += 1
         
         cont = input("\nDo you want a rematch? (y/n)")
         
-    overall_winner = "X" if scoreboard["x"] > scoreboard["o"] else "O"
-    print("\nFinal score: {}x{} for {}"
-          .format(scoreboard["x"], scoreboard["o"], overall_winner))
+        starting_player = opposite_player(starting_player)
+    
+    if scores[1] == scores[2]:
+        print("\nFinal score: {}x{} It's a draw!".format(scores[1], scores[2]))
+    else:
+        overall_winner = "Player 1" if scores[1] > scores[2] else "Player 2"
+        print("\nFinal score: {}x{} for {}"
+              .format(scores[1], scores[2], overall_winner))
+    
+    
     print("- GOODBYEE! -")
     
     
